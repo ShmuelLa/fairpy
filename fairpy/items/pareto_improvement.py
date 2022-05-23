d6be75a03351c1dba867d8a6efde7e785fe9247c
ShmueLa <shmuel516@gmail.com>
2022-05-23 03:53:21 +0300

    tests fix

--- fairpy/items/pareto_improvement.py
+++ fairpy/items/pareto_improvement.py
@@ -120,47 +120,6 @@ class ParetoImprovement:
         >>> pi = ParetoImprovement(allocations, G, items)
         >>> pi.find_pareto_improvement().is_complete_allocation()
         True
-
-        # Time consuming 
-        # >>> agent1= AdditiveAgent({"a": 100, "b": 10, "c": 50, "d": 100 ,"e": 70,"f": 100, "g": 300, "h": 40, "i": 30}, name="agent1")
-        # >>> agent2= AdditiveAgent({"a": 20, "b": 20, "c": 40, "d": 90 ,"e": 90,"f": 100, "g": 30, "h": 80, "i": 90}, name="agent2")
-        # >>> agent3= AdditiveAgent({"a": 10, "b": 30, "c": 30, "d": 40 ,"e": 180,"f": 100, "g": 300, "h": 20, "i": 90}, name="agent3")
-        # >>> agent4= AdditiveAgent({"a": 200, "b": 40, "c": 20, "d": 80 ,"e": 300,"f": 100, "g": 30, "h": 60, "i": 180}, name="agent4")
-        # >>> agent5= AdditiveAgent({"a": 50, "b": 50, "c": 10, "d": 60 ,"e": 90,"f": 100, "g": 300, "h": 120, "i": 180}, name="agent5")
-        # >>> agents = [agent1, agent2, agent3, agent4, agent5]
-        # >>> items = {'a','b','c','d','e','f','g','h','i'}
-        # >>> allocations = FractionalAllocation(agents, [{'a':0.0,'b':0.0,'c':1.0,'d':1.0,'e':0.0,'f':0.2,'g':0.0,'h':0.0,'i':0.0},{'a':0.0,'b':0.0,'c':0.0,'d':0.0,'e':0.8,'f':0.4,'g':0.0,'h':0.0,'i':0.0},{'a':0.0,'b':0.0,'c':0.0,'d':0.0,'e':0.0,'f':0.2,'g':1.0,'h':0.0,'i':0.0},{'a':1.0,'b':0.0,'c':0.0,'d':0.0,'e':0.2,'f':0.0,'g':0.0,'h':0.0,'i':0.0},{'a':0.0,'b':1.0,'c':0.0,'d':0.0,'e':0.0,'f':0.2,'g':0.0,'h':1.0,'i':1.0}])
-        # >>> G = nx.Graph()
-        # >>> G.add_node(agent1)
-        # >>> G.add_node(agent2)
-        # >>> G.add_node(agent3)
-        # >>> G.add_node(agent4)
-        # >>> G.add_node(agent5)
-        # >>> G.add_node('a')
-        # >>> G.add_node('b')
-        # >>> G.add_node('c')
-        # >>> G.add_node('d')
-        # >>> G.add_node('e')
-        # >>> G.add_node('f')
-        # >>> G.add_node('g')
-        # >>> G.add_node('h')
-        # >>> G.add_node('i')
-        # >>> G.add_edge(agent1, 'c')
-        # >>> G.add_edge(agent1, 'd')
-        # >>> G.add_edge(agent1, 'f')
-        # >>> G.add_edge(agent2, 'e')
-        # >>> G.add_edge(agent2, 'f')
-        # >>> G.add_edge(agent3, 'f')
-        # >>> G.add_edge(agent3, 'g')
-        # >>> G.add_edge(agent4, 'a')
-        # >>> G.add_edge(agent4, 'e')
-        # >>> G.add_edge(agent5, 'b')
-        # >>> G.add_edge(agent5, 'h')
-        # >>> G.add_edge(agent5, 'i')
-        # >>> G.add_edge(agent5, 'f')
-        # >>> pi = ParetoImprovement(allocations, G, items)
-        # >>> pi.find_pareto_improvement().is_complete_allocation()
-        # True
         
         Main algorithm 1 developper test
         >>> agent1 = AdditiveAgent({"a": 10, "b": 100, "c": 80, "d": -100}, name="agent1")
